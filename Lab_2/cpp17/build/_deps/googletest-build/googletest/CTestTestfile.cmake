0bdc6f49e390611e550b2b60c2182182ff832f15
mark <mark.matskevich2411@gmail.com>
2025-03-23 16:03:50 +0300

    Добавлена Lab_2

---
+++ Lab_2/cpp17/build/_deps/googletest-build/googletest/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /c/Users/37529/OneDrive/Рабочий стол/OS/Lab_2/cpp17/build/_deps/googletest-src/googletest
+# Build directory: /c/Users/37529/OneDrive/Рабочий стол/OS/Lab_2/cpp17/build/_deps/googletest-build/googletest
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
