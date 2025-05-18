bb3ea3a89d68290b20eb58e36dae1bf9a6b4fcd2
2342mark <113049108+2342mark@users.noreply.github.com>
2025-05-19 00:50:47 +0300

    Delete _temp_labs directory

--- _temp_labs/Lab_2/cpp17/build/_deps/googletest-build/googlemock/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /c/Users/37529/OneDrive/Рабочий стол/OS/Lab_2/cpp17/build/_deps/googletest-src/googlemock
-# Build directory: /c/Users/37529/OneDrive/Рабочий стол/OS/Lab_2/cpp17/build/_deps/googletest-build/googlemock
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("../googletest")
--- _temp_labs/Lab_4/build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/37529/OneDrive/Рабочий стол/OS/Новая папка
-# Build directory: C:/Users/37529/OneDrive/Рабочий стол/OS/Новая папка/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-include("C:/Users/37529/OneDrive/Рабочий стол/OS/Новая папка/build/tests[1]_include.cmake")
-subdirs("_deps/googletest-build")
