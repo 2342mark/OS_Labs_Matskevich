18eadf33cccc483fd46da74583cdf92ce837c300
2342mark <113049108+2342mark@users.noreply.github.com>
2025-05-19 00:49:13 +0300

    update lab5

--- _temp_labs/Lab_1/cpp17/build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/37529/OneDrive/Рабочий стол/OS/Lab1/cpp17
-# Build directory: C:/Users/37529/OneDrive/Рабочий стол/OS/Lab1/cpp17/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-include("C:/Users/37529/OneDrive/Рабочий стол/OS/Lab1/cpp17/build/tests[1]_include.cmake")
-subdirs("_deps/googletest-build")
--- _temp_labs/Lab_2/cpp17/build/_deps/googletest-build/googletest/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /c/Users/37529/OneDrive/Рабочий стол/OS/Lab_2/cpp17/build/_deps/googletest-src/googletest
-# Build directory: /c/Users/37529/OneDrive/Рабочий стол/OS/Lab_2/cpp17/build/_deps/googletest-build/googletest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
